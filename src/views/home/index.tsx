85ece19c1f548fce5e4b712064a3779487ba1f55
mayben <amedlisen67+1@gmail.com>
2022-02-01 00:22:44 -0800

    + edit home content and meta

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -30,7 +30,7 @@ export const HomeView: FC = ({ }) => {
 
     <div className="hero mx-auto p-4 min-h-16 py-4">
       <div className="hero-content flex flex-col max-w-lg">
-        <h1 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
+        <h1 className="text-5xl pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
           Scaffold Lite <span className='text-sm font-normal align-top text-slate-700'>v{pkg.version}</span>
         </h1>
         <h4 className="w-full max-w-md mx-auto text-center text-slate-300">
@@ -38,8 +38,8 @@ export const HomeView: FC = ({ }) => {
           Next.js, tailwind, wallet, web3.js, and more.
         </h4>
         <div className="mockup-code bg-primary m-0 pl-2 pr-16">
-          <pre data-prefix="$">
-            <code>npx i solana-dapp-scaffold </code>
+          <pre data-prefix=">">
+            <code>(Alpha) Start building on Solana with Next.js  </code>
           </pre>
         </div>
         <div>
